8104f9835d4964d795496530f815ab2104de49d0
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2024-02-03 17:33:17 +0530

    typedefs and resolvers

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone'; // Starting the server
 
+//data
+import db from './_db.js'; // Database
+
+//types
+import { typeDefs } from './schema.js'; // Schema
+
+//resolvers
+const resolvers = {
+    Query: {
+        games (){
+            return db.games;
+        },
+        reviews (){
+            return db.reviews;
+        },
+        authors (){
+            return db.authors;
+        }
+    }
+};
+
 //server Setup
 const server = new ApolloServer({
     // ... typeDefs and resolvers
+    typeDefs,
+    resolvers
 });
 
-const{ url }= await startStandaloneServer(server, { // Starting the server
+const { url }= await startStandaloneServer(server, { // Starting the server
     listen: {port: 4000} // Port number
 });
 
