39af5e4b4d31fefbc27a6dedc2fcdd882f54fcd8
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2024-02-04 01:25:02 +0530

    resolvers

--- index.js
+++ index.js
@@ -8,75 +8,7 @@ import db from './_db.js'; // Database
 import { typeDefs } from './schema.js'; // Schema
 
 //resolvers
-const resolvers = {
-    Query: {
-        games (){
-            return db.games;
-        },
-        game(_, args){ // Game by ID
-            return db.games.find((game) => game.id === args.id);
-        },
-        reviews (){
-            return db.reviews;
-        },
-        review(_, args){ // Review by ID
-            return db.reviews.find((review) => review.id === args.id);
-        },
-        authors (){
-            return db.authors;
-        },
-        author(_, args){ // Author by ID
-            return db.authors.find((author) => author.id === args.id);
-        }
-    },
-    Game: {
-        reviews(parent){
-            return db.reviews.filter((review) => review.game_id === parent.id);
-        }
-    },
-    Review: {
-        game(parent){
-            return db.games.find((game) => game.id === parent.game_id);
-        },
-        author(parent){
-            return db.authors.find((author) => author.id === parent.author_id);
-        }
-    },
-    Author: {
-        reviews(parent){
-            return db.reviews.filter((review) => review.author_id === parent.id);
-        }
-    },
-
-    //Mutation
-    Mutation: {
-        deleteGame(_, args){
-            db.games = db.games.filter((game) => game.id !== args.id);
-            return db.games;
-        },
-        addGame(_, args){
-            let Game = {
-                ...args.game,
-                id : Math.floor(Math.random() * 1000).toString()
-            }
-            db.games.push(Game);
-            return Game;
-        },
-        updateGame(_, args){
-            db.games = db.games.map((game) => {
-                if(game.id === args.id){
-                    return {
-                        ...game,
-                        ...args.edits
-                    }
-                }
-                return game;
-                
-            });
-            return db.games.find((game) => game.id === args.id);
-        }
-    }
-}
+import { resolvers } from './resolvers.js'; // Resolvers
 
 //server Setup
 const server = new ApolloServer({
